ffe4245b78ee1f59d2187238b7c90b66c50efc40
Erv <marcnicolas0915@gmail.com>
2022-11-28 18:09:06 -0500

    add static var

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,17 @@
 // Assignment code here
+var passwordValue = {
+  pwdLength: 0,
 
+  pwdLowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+  pwdUpperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+
+  pwdNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+  pwdCharacter: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+  "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +27,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
