e2ae02ce170ce7cad8a44212a6d66ada599684a4
Erv <marcnicolas0915@gmail.com>
2022-11-28 18:30:35 -0500

    added if criterias to var

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ var passwordValue = {
   pwdNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
 
   pwdCharacter: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
-  "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
+    "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
 
 }
 
@@ -28,3 +28,69 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  var result = " ";
+  var passwordLength = 0;
+  var upperCase = 0;
+  var lowerCase = 0;
+  var numbers;
+  var specialChar;
+
+  passwordLength = 0
+  passwordValue.pwdLength = 0;
+  result = " ";
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("How many characters for your password? \nPassword must be between 8 and 128 characters")
+
+    if (passwordLength === null) {
+      return ("Your password");
+    }
+    else {
+      if (isFinite(passwordLength)) {
+        alert("Please enter a number");
+        return "Your password";
+
+      }
+      else {
+        if (passwordLength < 8 || passwordLength > 128) {
+          alert("Password must be between 8 and 128 characters");
+          return "Your password"
+        }
+        else {
+          showPrompts();
+          while (passwordValue.pwdLength < passwordLength) {
+            if (pwdLowerCase === false && pwdUpperCase === false && pwdCharacter === false) {
+              alert("Please select at least one criteria of lowercase, uppercase numbers or special characters")
+              showPrompts();
+            }
+            else {
+              if (lowerCase === true && passwordValue.pwdLength < passwordLength) {
+                var lower = passwordValue.pwdLowerCase[Math.floor(Math.random() * 26)]
+                result = result + lower;
+                passwordValue.pwdLength++;
+
+              }
+              if (upperCase === true && passwordValue.pwdLength < passwordLength) {
+                var upper = passwordValue.pwdUpperCase[Math.floor(Math.random() * 26)]
+                result = result + upper;
+                passwordValue.pwdUpperCase++;
+              }
+              if (specialChar === true && passwordValue.pwdLength < passwordLength) {
+                var sc = passwordValue.pwdCharacter[Math.floor(Math.random() * 32)]
+                result = result + sc;
+                passwordValue.pwdCharacter++;
+              }
+              if (numbers === true && passwordValue.passwordLength < passwordLength) {
+                var numbers = passwordValue.pwdNumber[Math.floor(Math.random() * 10)]
+                result = result + numbers
+                passwordValue.pwdNumber++;
+              }
+            }
+          }
+        }
+      }
+    }
+
+  }
+}
